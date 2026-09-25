1f76734c3d93acfbd45f8355eb42aad96c9e8546
imzagoto <imanzgt12@gmail.com>
2026-09-25 11:43:38 +0700

    fix: replace BrowserRouter with HashRouter for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Sesuaikan dengan nama repositori atau buat relative path './'
-  base: "./",
+  // UBAH DARI "./" MENJADI NAMA REPO ANDA TERBUNGKUS SLASH:
+  base: "/personal-portfolio/",
   server: {
     host: "::",
     port: 8080,
